a7b96067b0de5f14f1946bc792eb87c36eefddea
rahulratra <rahulratra@192.168.1.8>
2019-07-18 19:24:38 -0400

    <spring-config>

---
+++ limits-service/src/main/java/com/in28Minutes/microservices/limitsservice/Configuration.java
@@ -0,0 +1,25 @@
+package com.in28Minutes.microservices.limitsservice;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+
+@ConfigurationProperties("limits-service")
+public class Configuration {
+	private int maximum;
+	private int minimum;
+	public int getMaximum() {
+		return maximum;
+	}
+	public int getMinimum() {
+		return minimum;
+	}
+	public void setMaximum(int maximum) {
+		this.maximum = maximum;
+	}
+	public void setMinimum(int minimum) {
+		this.minimum = minimum;
+	}
+	
+
+}
